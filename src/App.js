01b358b9b085737bc60ee209914a211d8dba033c
Oumaima901 <oumaimabk168@gmail.com>
2021-12-23 22:32:51 +0100

    adding the bootstrap and the shortid package and adding the style to our App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-
-function App() {
+export default class App extends Component {
+  constructor(){
+    super() 
+    this.state = {
+      todos:[],
+      editTask:false
+    }}
+    render() {
   return (
-    <div className="App">
-      
+    <div className="container">
+      <div className="row">
+          <div className="col-10 mx-auto col-md-8 mt-4"></div>
+          <h3 className="text-capitalize text-center">Todo Input</h3>
+          </div>
     </div>
   );
-}
+}}
 
-export default App;
+
