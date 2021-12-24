1ea10c03804c3eb941726c0918ae47e65288859e
Oumaima901 <oumaimabk168@gmail.com>
2021-12-24 21:59:28 +0100

    Adding the TaskItem

---
+++ src/TaskItem.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function TaskItem() {
+    return (
+       <li className="list-group-item text-capitalize d-flex justify-content-between my-2">
+           
+       </li>
+    )
+}
+
+export default TaskItem
+
