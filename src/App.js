0e5423adc3a75fb9bb237f8b9e71478fd9b0d8c4
Oumaima901 <oumaimabk168@gmail.com>
2021-12-24 23:22:40 +0100

    Adding the toggleComplete function

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { Component } from 'react'
+import ListTask from './components/ListTask'
 import TodoInput from './components/TodoInput'
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 export default class App extends Component {
   constructor(){
     super() 
@@ -8,16 +9,64 @@ export default class App extends Component {
       todos:[],
       editTask:false
     }}
-    render() {
-  return (
-    <div className="container">
-      <div className="row">
+    addTodo = (todo)=>{
+      //a new todo array that we can add a todo at the beginning
+      const newTodos =[todo,...this.state.todos];
+      //update the state
+      this.setState({
+         todos: newTodos
+      })
+    }
+    toggleComplete=(id)=>{
+      this.setState({
+          todos: this.state.todos.map(todo=>{
+            if(todo.id === id){
+             //suppose to update
+             return{
+               //keep the id the same and the text just change the complete (inverse it)
+               //or we can get rid of the id and the text cause it's the same and write ...todo, and the complete
+               id : todo.id,
+               text: todo.text,
+               complete: !todo.complete
+
+             }
+            }
+            else{
+              //we didn't change any thing
+              return todo;
+            }
+          })
+      })}
+      clearList = ()=>{
+        this.setState({
+          todos:[]
+        })
+      }
+     handleDelete = (id)=>{
+       //filter method is always return a new array 
+       //remove the item from the list
+        const filteredItems = this.state.todos.filter(todo =>
+          todo.id !== id)
+          this.setState({
+            todos:filteredItems
+          })
+
+     } 
+  render() {
+    return (
+      <div className="container">
+        <div className="row">
           <div className="col-10 mx-auto col-md-8 mt-4"></div>
           <h3 className="text-capitalize text-center">Todo Input</h3>
         <TodoInput addTodo ={this.addTodo}/>
-          </div>
-    </div>
-  );
-}}
-
+        
+        <ListTask handleDelete={this.handleDelete} clearList={this.clearList} todos={this.state.todos} toggleComplete={this.toggleComplete}/>
+        </div>
+      </div>
+      
+    )
+  }
+}
+//turning the todos array into string and rendering the string in the json.stringify
+//{JSON.stringify(this.state.todos)}
 
